5ee18e08a1e14f7d3412a67dfe5a4d6cbb837e6c
iuri-pdista <correaiuri86@gmail.com>
2020-09-13 15:27:38 -0300

    Created basic functions and structure

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::fs::File;
+use std::string::String;
+use std::io;
+use std::io::prelude::*;
+
+fn main() {}
+
+fn write_in_file () -> io::Result<()> {
+    Ok(())
+}
+
+fn trasnlate () {}
+
