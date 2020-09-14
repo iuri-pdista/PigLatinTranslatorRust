d757b4f207193fdbb6607b4e85f87cffce888b12
iuri-pdista <correaiuri86@gmail.com>
2020-09-14 11:34:53 -0300

    Created main logic

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use std::fs::File;
+use std::io::prelude::*;
+use std::fs::OpenOptions;
 use std::string::String;
 use std::io;
-use std::io::prelude::*;
+use std::convert::From;
 
-fn main() {}
-
-fn write_in_file () -> io::Result<()> {
-    Ok(())
+fn main() {
+    let mut nt_word = String::new();
+    io::stdin()
+        .read_line(&mut nt_word)
+        .expect("Failed to read line");
+    println!("{:?}", nt_word);
+    {
+        let nt_word = nt_word;
+        let mut nt_word_in_char: Box<Vec<char>> = Box::new(nt_word.chars().collect::<Vec<char>>());
+        let t_word_in_char: Box<Vec<char>> = translate(nt_word_in_char);
+        let _result: io::Result<()> = write_in_file(t_word_in_char);
+    }
 }
-
-fn trasnlate () -> Box<Vec<char>>{}
-
